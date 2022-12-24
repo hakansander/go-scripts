a47b5ac8aba3b536148e18cbb070a12d4c621fab
hakansander <hakansander@gmail.com>
2022-12-24 10:57:17 +0300

    init commit

---
+++ concurrency/buffered_channel.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	bufferedChannel := make(chan int, 5)
+	done := make(chan bool)
+
+	go func(bufferedChannel chan int, done chan bool) {
+		//when the buf channel closed loop will be broken
+		for val := range bufferedChannel {
+			fmt.Println(val)
+		}
+
+		fmt.Println("channel closed")
+		done <- true
+	}(bufferedChannel, done)
+
+	bufferedChannel <- 1
+	bufferedChannel <- 2
+	bufferedChannel <- 3
+	bufferedChannel <- 4
+	bufferedChannel <- 5
+	bufferedChannel <- 6
+	bufferedChannel <- 7
+	bufferedChannel <- 8
+	bufferedChannel <- 9
+
+	close(bufferedChannel)
+}
